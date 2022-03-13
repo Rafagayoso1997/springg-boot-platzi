f75e3c88eab402d10b693a0f7447f84a8e9644e6
Rafa Gayoso <gayoso0597@gmail.com>
2022-03-13 17:43:53 +0100

    first commit

---
+++ src/main/java/com/gsoft/SpringPlatzi/SpringPlatziApplication.java
@@ -0,0 +1,13 @@
+package com.gsoft.SpringPlatzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlatziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlatziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsoft/SpringPlatzi/SpringPlatziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsoft.SpringPlatzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlatziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
